dd5a8f8420da08181cce52238e6230a3ce3a6c58
fiyahraa <luthfiyahza19@gmail.com>
2021-11-17 22:26:45 +0700

    menambahkan file program

---
+++ Bilangan Acak/praktikum1.py
@@ -0,0 +1,7 @@
+#angka = range (0,5)
+
+n = float (input("masukan jumblah N ="))
+
+while n < 0.4 :
+    n +=0.1 
+    print (n)
---
+++ Menghitung laba/praktikum3.py
@@ -0,0 +1,22 @@
+modal = 100000000
+#laba = modal/0.1
+
+
+
+for i in range (1,3):
+    laba = modal*0 
+    print ("laba bulan ke :",i,"=",laba)
+
+for z in range (3,5):
+    laba = 0.01 * modal
+    print ("laba bulan ke :",z,"=",laba)
+
+for k in range (5,8) :
+    laba = 0.05 * modal
+    print ("laba bulan ke :",k,"=",laba)   
+
+for p in range (8,9) :
+    laba = 0.03 * modal 
+    print ("laba bulan ke :",p,"=",laba)
+laba = 6500000
+print ("total laba" ,laba)
